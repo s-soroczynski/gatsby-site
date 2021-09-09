59c256349b88d2d765ebf25365ca96a1e6f5aea0
Sebastian Soroczyński <s.soroczynski@artgeist.com>
2021-09-09 20:06:50 +0200

    add dynamic images

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     plugins: [
       "gatsby-plugin-image",
       "gatsby-plugin-sharp",
+      "gatsby-transformer-sharp",
       {
         resolve: "gatsby-source-filesystem",
         options: {
@@ -12,5 +13,6 @@ module.exports = {
           path: `${__dirname}/blog`,
         }
       },
+      "gatsby-plugin-mdx",
     ],
   };
